8032ff097655700157ec46eae882a07e74470b75
Peter Wang <Peter.WANG3@cn.bosch.com>
2020-03-26 15:38:25 +0800

    Fix Android build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)/..
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS    := -std=c99 -O2 -W -Wall -pthread -pipe $(COPT)
+LOCAL_CFLAGS    := -std=c99 -O2 -W -Wall -pthread -pipe -DMG_ENABLE_THREADS -DMG_ENABLE_HTTP_WEBSOCKET=0 $(COPT)
 LOCAL_MODULE    := dlc
-LOCAL_SRC_FILES := dlc_server.c src/mongoose.c
+LOCAL_SRC_FILES := dlc_server.c mongoose/mongoose.c cJSON/cJSON.c
 
 include $(BUILD_EXECUTABLE)
